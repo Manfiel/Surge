5c34cc834922b05abc368c09d86d78a4cabfa395
Manfiel <33372152+Manfiel@users.noreply.github.com>
2024-01-21 11:28:13 +0800

    Update icbcelife_tuniu.js

--- Scripts/icbcelife_tuniu.js
+++ Scripts/icbcelife_tuniu.js
@@ -1,4 +1,4 @@
-let regex_1='<body>'
-let body_1='<body><a href="https://m.tuniu.com/m2015/mpChannel/index" style="font-size:25px;">点我跳转门票</a>'
+var regex_1='<body>'
+var body_1='<body><a href="https://m.tuniu.com/m2015/mpChannel/index" style="font-size:25px;">点我跳转门票</a>'
 let body=$response.body.replace(regex_1,body_1)
 $done({body});
